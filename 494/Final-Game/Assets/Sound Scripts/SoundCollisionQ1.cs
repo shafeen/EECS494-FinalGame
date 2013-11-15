07abe7adb94573d420e1943f170d4a39051ab96b
Shafeen M <sodl02@gmail.com>
2013-11-14 22:35:42 -0500

    Added a general sound script that I will expand on.
    Made separate folders to organize assets and scripts for sounds. Theres
    also a new scene called SoundTest.unity

---
+++ 494/Final-Game/Assets/Sound Scripts/SoundCollisionQ1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundCollisionQ1 : MonoBehaviour 
+{
+
+	// set this via drag and drop
+	public GameObject soundSrcObj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("Sound Queued!!\n");
+		//GameObject.Find ("SoundSource1").audio.Play();
+		soundSrcObj.audio.Play ();
+	}
+		
+}
