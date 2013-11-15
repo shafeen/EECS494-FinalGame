cad7f35a583072719ecfd94178482259baf26b68
David Zilli <davidzilli@gmail.com>
2013-11-15 10:38:37 -0500

    added crystals and semi-working flashlight which triggers them

---
+++ 494/Final-Game/Assets/Prefabs/Crystal/CrystalLightScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalLightScript : MonoBehaviour {
+
+	private Light crystal_light;
+	public float min_light_intensity = 0;
+	public static float max_light_intensity = 5;
+	public float local_max_light_intensity = 4;
+	public float light_charge_delta = 0.05f;
+	private float light_discharge_delta = 0.01f;
+	private float charge_neighbor_threshold = 2.0f;
+	private float neighbor_charge_step = 0.20f;
+	public bool is_being_charged_by_flashlight = false;
+	public bool is_being_charged_by_radiance = false;
+
+	// Use this for initialization
+	void Start () {
+		crystal_light = transform.GetChild(0).GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//The only difference between being charged by the flashlight and by another crystal, is that
+		//the flashlight will reset your max brightness to the full allowed
+		//local max brightness is how bright a particular crystal is allowed to be
+		//max brightness is more like a constant, the brightest any crystal can be
+		if (is_being_charged_by_flashlight) {
+			local_max_light_intensity = max_light_intensity;
+			if (crystal_light.intensity < local_max_light_intensity) {
+				crystal_light.intensity += light_charge_delta;
+			}
+		} else if (is_being_charged_by_radiance) {
+			if (crystal_light.intensity < local_max_light_intensity) {
+				crystal_light.intensity += light_charge_delta;
+			}
+		} else {
+			if (crystal_light.intensity > min_light_intensity) {
+				crystal_light.intensity -= light_discharge_delta;
+			}
+		}
+
+		if (crystal_light.intensity > charge_neighbor_threshold) {
+			ChargeNeighbors();
+		}
+		
+		// Reset at the end of every update so the flashlight has to be kept on the crystal to set this true
+		is_being_charged_by_flashlight = false;
+		is_being_charged_by_radiance = false;
+	}
+
+	void ChargeNeighbors() {
+		//Get a list of all objects within our light radius
+		Collider[] colliders = Physics.OverlapSphere(transform.position, transform.GetChild(0).GetComponent<Light>().range);
+
+		//Filter by the tag "Crystal"
+		//If the neighboring crystal is dimmer than you, charge him up, but set his max brightness to a little less
+		//than yours
+		foreach(Collider col in colliders) {
+			if (col.tag == "Crystal" && col.gameObject != gameObject) {
+				CrystalLightScript neighbor_crystal = col.transform.GetComponent<CrystalLightScript>();
+				Light neighbor_crystal_light = col.transform.GetChild(0).GetComponent<Light>();
+				if (neighbor_crystal_light.intensity < crystal_light.intensity) {
+					neighbor_crystal.local_max_light_intensity = local_max_light_intensity - neighbor_charge_step;
+					neighbor_crystal.is_being_charged_by_radiance = true;
+				}
+			}
+		}
+	}
+}
---
+++ 494/Final-Game/Assets/Prefabs/Flashlight/FlashlightRaycast.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlightRaycast : MonoBehaviour {
+	public float ray_range;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		Ray lightRay = new Ray(transform.position, transform.forward);
+
+		Debug.DrawRay(transform.position, transform.forward * ray_range);
+
+		if (Physics.Raycast(lightRay, out hit, ray_range)) {
+
+			if (hit.collider.tag == "Crystal") {
+				CrystalLightScript crystal = hit.collider.transform.GetComponent<CrystalLightScript>();
+				if (crystal) {
+					crystal.is_being_charged_by_flashlight = true;
+				}
+			}
+		}
+	}
+}
