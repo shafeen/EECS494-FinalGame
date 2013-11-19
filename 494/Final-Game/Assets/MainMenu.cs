a35b09be8cb834181f733266fb7787627d8a868c
Bradley Menchl <brmenchl@umich.edu>
2013-11-19 01:25:00 -0500

    Simple main menu/settings menu.  pushCart now belongs to cart objects

---
+++ 494/Final-Game/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	public GUIStyle titleStyle;
+	void Start() {
+	}
+	void OnGUI () {
+		GUI.Label (new Rect (Screen.width/2-50, 40, 100, 100), "Time Out!", titleStyle);
+		if(GUI.Button(new Rect(Screen.width/2-50,120,100,30), "Start")) {
+			Application.LoadLevel("Bedroom");
+		}
+		if(GUI.Button(new Rect(Screen.width/2-50,150,100,30), "Settings")) {
+			Application.LoadLevel("Settings");
+		}
+		if(GUI.Button(new Rect(Screen.width/2-50,180,100,30), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
---
+++ 494/Final-Game/Assets/Settings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Settings : MonoBehaviour {
+	public GUIStyle settingsStyle;
+	void OnGUI () {
+		GUI.Label (new Rect (Screen.width/2-50, 40, 100, 100), "There will be settings here, lots of them.", settingsStyle);
+		if(GUI.Button(new Rect(Screen.width/2-50,120,100,30), "Go Back")) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
