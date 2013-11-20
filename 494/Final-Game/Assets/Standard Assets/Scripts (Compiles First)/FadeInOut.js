797c297431aeca5dc240012ee81dad9244ae1f74
Bradley Menchl <brmenchl@umich.edu>
2013-11-19 19:07:59 -0500

    Working minecart light source/fade to black

---
+++ 494/Final-Game/Assets/Standard Assets/Scripts (Compiles First)/FadeInOut.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public var fadeOutTexture : Texture2D;
+ 
+var drawDepth = -1000;
+public var timeLeft : float;
+public var timeLimit : float;
+private var alpha :float = 0; 
+ 
+public var fadeSpeed:float;
+ 
+ 
+function OnGUI(){
+	alpha = timeLeft/timeLimit;
+
+	GUI.color.a = alpha;
+ 
+	GUI.depth = drawDepth;
+ 
+	GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeOutTexture);
+}
+ 
+//--------------------------------------------------------------------
+
