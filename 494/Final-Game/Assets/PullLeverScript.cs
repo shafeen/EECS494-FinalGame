6cb1410a90c0b1e9a818792b1256a3dbfb398fd4
David Zilli <davidzilli@gmail.com>
2013-11-22 15:52:52 -0500

    moved scenes into their own folder in assset

---
+++ 494/Final-Game/Assets/PullLeverScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PullLeverScript : MonoBehaviour {
+
+	public Transform portcullis;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseEnter() {
+		animation.Play("Pull Lever Down");
+		portcullis.animation.Play("Raise Portcullis");
+	}
+}
