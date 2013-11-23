8303bbb68731fd0be0bbbc790865937006a014fe
David Zilli <davidzilli@gmail.com>
2013-11-22 23:28:42 -0500

    changes to portcullis puzzle test scene and some prefabs

--- 494/Final-Game/Assets/PullLeverScript.cs
+++ 494/Final-Game/Assets/PullLeverScript.cs
@@ -4,19 +4,31 @@ using System.Collections;
 public class PullLeverScript : MonoBehaviour {
 
 	public Transform portcullis;
+	private Transform lever;
+	private Transform handle;
+	private bool pulled = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		lever = transform.FindChild("Lever");
+		handle = lever.FindChild("Handle");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if(Input.GetButtonDown("A_1") || Input.GetMouseButtonDown(0)){
+			Debug.Log("Clicking");
+			click();
+		}
 	
 	}
 
-	void OnMouseEnter() {
-		animation.Play("Pull Lever Down");
-		portcullis.animation.Play("Raise Portcullis");
+	void click() {
+		if (!pulled && transform.FindChild("Object_Highlight").gameObject.GetComponent<Light>().enabled) {
+			handle.animation.Play("Pull Lever Down");
+			portcullis.FindChild("Portcullis Model").animation.Play("Raise Portcullis");
+			pulled = true;
+		}
 	}
 }
