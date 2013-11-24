7b51497451962d9173a0a51d88bd936fa9e4ccc4
Bradley Menchl <brmenchl@umich.edu>
2013-11-24 18:09:49 -0500

    Head Bob Fix

--- 494/Final-Game/Assets/Camera Scripts/StaticScript.cs
+++ 494/Final-Game/Assets/Camera Scripts/StaticScript.cs
@@ -7,35 +7,29 @@ public class StaticScript : MonoBehaviour {
 	public int columns = 2;
 	public int rows = 3;
 	public int fps = 10;
-	public float multiplier;
 	private Vector2 size;
 	private Renderer myRenderer;
 	private int lastIndex = -1;
-	private GameObject player;
 	private Vector3 fwd;
 	private Color fadeLevel;
 	private RespawnTimer timer;
 	void Start (){
-		size = new Vector2 (1.0f / columns ,1.0f / rows);
+		timer = GameObject.Find("Player").GetComponent<RespawnTimer>();
 		myRenderer = renderer;
 		if(myRenderer == null) enabled = false;
+		size = new Vector2 (1.0f / columns ,1.0f / rows);
 		myRenderer.material.SetTextureScale ("_MainTex", size);
-		player = GameObject.Find("Player_Cam");
-		fwd = player.transform.TransformDirection(Vector3.forward);
-		transform.position = player.transform.position + fwd;
-		transform.rotation = player.transform.rotation;
+
 		fadeLevel = myRenderer.material.color;
 		fadeLevel.a = 0;
 		myRenderer.material.color = fadeLevel;
-		timer = GameObject.Find("Player").GetComponent<RespawnTimer>();
 	}
 	void Update() {
 		fadeLevel = myRenderer.material.color;
-		int index = (int)(Time.time * fps) % (columns * rows);
-
 		fadeLevel.a = (1-timer.timeLeft/timer.timeLimit);	
 		myRenderer.material.color = fadeLevel;
 
+		int index = (int)(Time.time * fps) % (columns * rows);
 		if(index != lastIndex) {
 			Vector2 offset = new Vector2(x*size.x, 1-(size.y*y));
 			x++;
--- 494/Final-Game/Assets/Standard Assets/Scripts (Compiles First)/HeadBob.js
+++ 494/Final-Game/Assets/Standard Assets/Scripts (Compiles First)/HeadBob.js
@@ -4,20 +4,25 @@
  public var bobSpeed = 0.18; 
  public var bobAmplitude = 0.2; 
  private var midpoint = 0.0;
- 
+ private var prevPosition : Vector3;
 function Start () {
 	midpoint = transform.localPosition.y;
+  prevPosition = transform.position;
 }
-
+  function FixedUpdate () {
+    var vel = ((transform.position - prevPosition)/Time.deltaTime);
+    if(vel.magnitude < 1) {timer = 0.0;}
+    prevPosition = transform.position;
+  }
  function Update () { 
     var sinTime = 0.0; 
     var horizontal = Mathf.Abs(Mathf.Clamp(Input.GetAxis("L_XAxis_1") + Input.GetAxis("Horizontal"), -1, 1));
     var vertical = Mathf.Abs(Mathf.Clamp(Input.GetAxis("L_YAxis_1") + Input.GetAxis("Vertical"), -1,1));
-    if (horizontal == 0 && vertical == 0){timer = 0.0;} 
+    if ((horizontal == 0 && vertical == 0)){timer = 0.0;} 
     else { 
-       sinTime = Mathf.Sin(timer); 
+       sinTime = Mathf.Sin(timer);
        timer += bobSpeed;
-       timer = timer % (Mathf.PI * 2); 
+       timer = timer % (Mathf.PI * 2);
     } 
     if (sinTime != 0) { 
        var changeFromMid = sinTime * bobAmplitude; 
