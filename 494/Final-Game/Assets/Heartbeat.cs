3638ac061d3842aaea71c7fe9fa484177cbbca06
David Zilli <davidzilli@gmail.com>
2013-11-25 19:28:27 -0500

    Added torch scripts, some sound affects, accellerating heartbeat on blackout

---
+++ 494/Final-Game/Assets/Heartbeat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Heartbeat : MonoBehaviour {
+	private RespawnTimer timer;
+	private float delay = 0.0f;
+	private bool play;
+
+	// Use this for initialization
+	void Start () {
+		timer = GameObject.Find("Player").GetComponent<RespawnTimer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		delay -= Time.deltaTime;
+		if (timer.timeLeft < timer.timeLimit) {
+			if (delay <= 0) {
+				audio.Play();
+				delay =	Mathf.Max((timer.timeLeft/timer.timeLimit) * 1.0f, 0.4f);
+//				delay = 0;
+				Debug.Log("Heartbeat delay is " + delay);
+			}
+		}
+	}
+}
---
+++ 494/Final-Game/Assets/KillTorches.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillTorches : MonoBehaviour {
+
+	public TorchSet[] torchSets;
+	private float time = 0.0f;
+	private bool start_scene = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (start_scene) {
+			time += Time.deltaTime;
+
+			foreach (TorchSet ts in torchSets) {
+				if (ts.delay <= time) {
+					foreach (Transform torch in ts.torches) {
+						torch.FindChild("Fire").active = false;
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Player") {
+			start_scene = true;
+		}
+	}
+
+	[System.Serializable]
+	public class TorchSet {
+
+		public Transform[] torches;
+		public float delay;
+	}
+}
