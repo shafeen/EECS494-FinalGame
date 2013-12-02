07d065373690d62cca7f7334217fd47675ab592c
David Zilli <davidzilli@gmail.com>
2013-12-02 18:58:46 -0500

    added connection between crystal to crystal scene and great hall

---
+++ 494/Final-Game/Assets/LevelChange.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChange : MonoBehaviour {
+
+	public string new_level;
+
+	void OnTriggerEnter() {
+		Application.LoadLevel(new_level);
+	}
+
+
+}
