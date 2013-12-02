b35c514ad91cad26d8367df01f94a27e37dbc9d9
Bradley Menchl <brmenchl@umich.edu>
2013-12-02 16:26:25 -0500

    Minecart Fixes

--- 494/Final-Game/Assets/PushCart.cs
+++ 494/Final-Game/Assets/PushCart.cs
@@ -2,31 +2,41 @@
 using System.Collections;
 
 public class PushCart : MonoBehaviour {
-	private Vector3 cartCenter = Vector3.zero;
 	private bool attached = false;
 	private RaycastHit hit;
+	private RaycastHit hit2;
 	private Vector3 fwd;
 	public float attachRange;
-	private Transform cartTransform;
+	private GameObject cart;
+	private CharacterMotor playerChMotor;
+	private MouseLook playerMLook;
+	private MouseLook camMLook;
+	void Start(){
+		playerChMotor = GetComponent<CharacterMotor>();
+		playerMLook = GetComponent<MouseLook>();
+		camMLook = GameObject.FindWithTag("MainCamera").GetComponent<MouseLook>();
+
+	}
 	void cartCheck(){
 		if(attached){
 			attached = false;
 		}
 		else if(Physics.Raycast(transform.position, fwd, out hit, attachRange)) {
 			if(hit.collider.gameObject.tag == "cart"){
+
 				attached = true;
-				cartTransform = hit.collider.gameObject.transform;
+
 			}
 		}
 	}
 	void Update(){
 		fwd = transform.TransformDirection(Vector3.forward);
+		Vector3 fwdC = GameObject.FindWithTag("cart").transform.TransformDirection(Vector3.forward);
 		Debug.DrawRay(transform.position, fwd, Color.red, 0.0f, false);
+		Debug.DrawRay(GameObject.FindWithTag("cart").transform.position, fwdC*20, Color.red, 0.0f, false);
+		Debug.DrawRay(GameObject.FindWithTag("cart").transform.position, fwdC*-20, Color.red, 0.0f, false);
 		if(Input.GetMouseButtonDown(0)){
 			cartCheck();
-		}
-		if(attached){
-			cartTransform.position = transform.position;
 		}
 
 	}
