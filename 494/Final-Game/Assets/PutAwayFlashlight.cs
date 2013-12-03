ffeb8433973ebf041e163b4687235a52bd938bb5
David Zilli <davidzilli@gmail.com>
2013-12-02 19:41:49 -0500

    Added animation to raise and lower flashlight

---
+++ 494/Final-Game/Assets/PutAwayFlashlight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PutAwayFlashlight : MonoBehaviour {
+
+	private bool flashlight_is_up = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown("B_1") || Input.GetKeyDown(KeyCode.Space)) {
+			if (flashlight_is_up) {
+				animation.Play("Lower_Flashlight");
+				flashlight_is_up = false;
+			} else {
+				animation.Play("Raise_Flashlight");
+				flashlight_is_up = true;
+			}
+		}
+
+	}
+}
