ccf6354611bc8b63de9c3231ed7b9fca84c314b2
Bradley Menchl <brmenchl@umich.edu>
2013-12-02 19:29:21 -0500

    Let's hope this works

---
+++ 494/Final-Game/Assets/Prefabs/Minecart/PushScript.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushScript : MonoBehaviour {
+	public float attachRange = 1;
+	private GameObject player;
+	private GameObject player_cam;
+	private RaycastHit hit;
+	private Vector3 playerFwd;
+	private Vector3 cartFwd;
+	private WheelTurn wheelTurn;
+	private RespawnTimer respawn;
+
+	void Start () {
+		player = GameObject.Find("Player");
+		player_cam = GameObject.Find("Player/Player_Cam");
+		respawn = player.GetComponent<RespawnTimer>();
+		wheelTurn = transform.Find("Wheel Container").GetComponent<WheelTurn>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		cartFwd = transform.TransformDirection(Vector3.forward);
+		playerFwd = player.transform.TransformDirection(Vector3.forward);
+		if(wheelTurn.isActive() && (Input.GetButtonDown("B_1") || Input.GetMouseButtonDown(1) || respawn.getTimeLeft() < 3)) {
+			GameObject.Find("Player/Player_Cam/Flashlight").GetComponent<Light>().enabled = true;
+			GameObject.Find("Player/Player_Cam/Flashlight").GetComponent<FlashlightRaycast>().enabled = true;
+			GameObject.Find("Player/Player_Cam/Flashlight/Wide_light").GetComponent<Light>().enabled = true;
+
+
+			player.transform.parent = null;
+
+			player.GetComponent<MouseLook>().sensitivityX = 15.0f;
+			player.GetComponent<MouseLook>().sensitivityY = 15.0f;
+			player.GetComponent<CharacterMotor>().enabled = true;
+			player.GetComponent<CharacterMotor>().canControl = true;
+			player_cam.transform.rotation = player.transform.rotation;
+
+			wheelTurn.deactivate();
+
+		}
+		if(Input.GetMouseButtonDown(0) || Input.GetButtonDown("A_1") && !wheelTurn.isActive()) {
+			attachToCart ();
+		}
+	}
+	void attachToCart(){
+		if(Physics.Raycast(player.transform.position, playerFwd, out hit, attachRange)) {
+			if(hit.collider.gameObject.name == name) {
+				if(Vector3.Angle(playerFwd,cartFwd) < 20){
+					transform.rotation = player.transform.rotation;
+					player.transform.parent = transform;
+					GameObject.Find("Player/Player_Cam/Flashlight").GetComponent<Light>().enabled = false;
+					GameObject.Find("Player/Player_Cam/Flashlight").GetComponent<FlashlightRaycast>().enabled = false;
+					GameObject.Find("Player/Player_Cam/Flashlight/Wide_light").GetComponent<Light>().enabled = false;
+
+
+					player.GetComponent<MouseLook>().sensitivityX = 0.0f;
+					player.GetComponent<MouseLook>().sensitivityY = 0.0f;
+					player.GetComponent<CharacterMotor>().enabled = false;
+					player.GetComponent<CharacterMotor>().canControl = false;
+					wheelTurn.activate();
+				}
+			}
+		}
+	}
+}
---
+++ 494/Final-Game/Assets/Prefabs/Minecart/WheelTurn.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelTurn : MonoBehaviour {
+	public WheelCollider FRColl;
+	public WheelCollider FLColl;
+	public WheelCollider BRColl;
+	public WheelCollider BLColl;
+	public Transform FRWheel;
+	public Transform FLWheel;
+	public Transform BRWheel;
+	public Transform BLWheel;
+
+	private float steer_max = 20;
+	private float torque_max = 10;
+	private float brake_max = 20;
+
+	private float steer = 0;
+	private float torque = 0;
+	private float brake = 0;
+	private float back = 0;
+	private float forward = 0;
+	private bool reverse = false;
+	private float speed = 0;
+
+	private bool activated = false;
+	private Transform cart;
+
+	public void Start(){
+		cart = transform.parent;
+		Vector3 temp = new Vector3(0,-2,0);
+		cart.rigidbody.centerOfMass = temp;
+	}
+	public void activate(){
+		activated = true;
+	}
+	public void deactivate(){
+		activated = false;
+	}
+	public bool isActive(){
+		return activated;
+	}
+	void FixedUpdate () {
+
+		speed = cart.rigidbody.velocity.sqrMagnitude;
+
+		if(activated) {
+			steer = Mathf.Clamp(Input.GetAxis("L_XAxis_1") + Input.GetAxis("Horizontal"),-1,1);
+			forward = Mathf.Clamp(Input.GetAxis("Vertical") + Input.GetAxis("L_YAxis_1"), 0, 1);
+			back = -1 * Mathf.Clamp(Input.GetAxis("Vertical")+Input.GetAxis("L_YAxis_1"), -1, 0);
+			if(speed < 0.01) {
+				if(back > 0) {
+					reverse = true;
+				}
+				if(forward > 0) {
+					reverse = false;
+				}
+			}
+			
+			if(reverse) {
+				torque = -1 * back;
+				brake = forward;
+			}
+			else {
+				torque = forward;
+				brake = back;
+			}
+		}
+		else {
+			steer = 0;
+			torque = 0;
+			brake = 1;
+		}
+		FRColl.steerAngle = steer * steer_max;
+		FLColl.steerAngle = steer * steer_max;
+		BRColl.motorTorque = torque * torque_max;
+		BLColl.motorTorque = torque * torque_max;
+		BRColl.brakeTorque = brake * brake_max;
+		BLColl.brakeTorque = brake * brake_max;
+
+		FRWheel.Rotate(0,FRColl.rpm * -6 * Time.deltaTime,0, Space.Self);
+		FLWheel.Rotate(0,FLColl.rpm * -6 * Time.deltaTime,0, Space.Self);
+		BRWheel.Rotate(0,BRColl.rpm * -6 * Time.deltaTime,0, Space.Self);
+		BLWheel.Rotate(0,BLColl.rpm * -6 * Time.deltaTime,0, Space.Self);
+	}
+}
