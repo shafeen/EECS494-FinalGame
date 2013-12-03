82bec74ea5555b6187b3fbabd0274aca48ec3eec
David Zilli <davidzilli@gmail.com>
2013-12-03 03:51:42 -0500

    Basics of bridge puzzle in place, waiting on updated mine cart to complete

---
+++ 494/Final-Game/Assets/CrystalBridgeScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrystalBridgeScript : MonoBehaviour {
+
+	private bool bridge_raised = false;
+
+	public Transform[] crystals;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.Find("light").GetComponent<Light>().intensity > 3.0) {
+			RaiseBridge();
+		} else if (AllCrystalsLit()) {
+			RaiseBridge();
+		} else {
+			LowerBridge();
+		}
+
+	}
+
+	void RaiseBridge() {
+		if (!bridge_raised) {
+			transform.Find("Bridge/Bridge_beam").animation.Play("Raise_Bridge");
+			bridge_raised = true;
+		}
+	}
+
+	void LowerBridge() {
+		if (bridge_raised) {
+			transform.Find("Bridge/Bridge_beam").animation.Play("Lower_Bridge");
+			bridge_raised = false;
+		}
+	}
+
+	bool AllCrystalsLit() {
+		if (crystals.Length == 0) {
+			return false;
+		}
+
+		foreach (Transform t in crystals) {
+			if (t.GetComponent<Light>().intensity < 3.0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
