03bd5a90df94957b31211d94a9a295a4df16fb70
Bradley Menchl <brmenchl@umich.edu>
2013-12-03 23:16:14 -0500

    Win script added

--- 494/Final-Game/Assets/GUI Scripts/WinScript.cs
+++ 494/Final-Game/Assets/GUI Scripts/WinScript.cs
@@ -8,28 +8,28 @@ public class WinScript : MonoBehaviour {
 	private bool winning;
 	private GUITexture fader;
 	void Awake () {
-		player = GameObject.Find("Player");
+		player = GameObject.FindWithTag("Player");
 		fader = (GameObject.Find("ScreenFader").GetComponent<GUITexture>()as GUITexture);
 		fader.guiTexture.enabled = false;
-		fader.guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+		fader.guiTexture.pixelInset = new Rect(-5000, -5000, 10000, 10000);
 	}
 
 
 	// Update is called once per frame
 	void Update () {
-		fader.guiTexture.pixelInset = new Rect(0f, 0f, Screen.width, Screen.height);
+		fader.guiTexture.pixelInset = new Rect(-5000, -5000, 10000, 10000);
 		fwd = player.transform.TransformDirection(Vector3.forward);
 		if(winning) {
 			fader.guiTexture.enabled = true;
 			fader.guiTexture.color = Color.Lerp(fader.guiTexture.color, Color.white, 0.4f*Time.deltaTime);
 			print(fader.guiTexture.color.a);
-			if(fader.guiTexture.color.a >= 0.70f){
-				Application.LoadLevel("Bedroom");
+			if(fader.guiTexture.color.a >= 0.60f){
+				Application.LoadLevel("BedroomLevel");
 			}
 		}
 		if((Input.GetButtonDown("A_1") || Input.GetMouseButtonDown(0))){
 			if(Physics.Raycast(player.transform.position, fwd, out hit, 3.0f)) {
-				if(hit.collider.gameObject.name == name) {
+				if(hit.collider.gameObject == gameObject) {
 					fader.guiTexture.color = Color.clear;
 					winning = true;
 				}
