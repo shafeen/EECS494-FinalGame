b6ceaab557fa97757f425b480e45012e67cb5281
David Zilli <davidzilli@gmail.com>
2013-12-04 20:27:59 -0500

    Inspect raycast comes from flashlight IF you have one, else from the center of the camera

--- 494/Final-Game/Assets/Prefabs/Player/CheckInspectRange.cs
+++ 494/Final-Game/Assets/Prefabs/Player/CheckInspectRange.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class CheckInspectRange : MonoBehaviour {
 	private float ray_range = 3.0f;
+	private Transform flashlight;
 	// Use this for initialization
 	void Start () {
-	
+		flashlight = transform.Find("Flashlight");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		RaycastHit hit;
-		Ray lightRay = new Ray(transform.position, transform.forward);
-		Debug.DrawRay(transform.position, ray_range * transform.forward, Color.yellow, 0.0f, false);
+		Ray lightRay;
+		if (flashlight) {
+			lightRay = new Ray(flashlight.position, flashlight.forward);
+			Debug.DrawRay(flashlight.position, ray_range * flashlight.forward, Color.yellow, 0.0f, false);
+		} else {
+			lightRay = new Ray(transform.position, transform.forward);
+			Debug.DrawRay(transform.position, ray_range * transform.forward, Color.yellow, 0.0f, false);
+		}
 		if (Physics.Raycast(lightRay, out hit, ray_range)) {
 			
 			if (hit.collider.tag == "focusObject") {
