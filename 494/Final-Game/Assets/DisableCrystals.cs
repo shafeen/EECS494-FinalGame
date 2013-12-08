d4f382a1f9578229077d5d94e8b4ccf807f1fc36
David Zilli <davidzilli@gmail.com>
2013-12-08 17:36:48 -0500

    Added disable and enable fields to clusters of crystals to boost performance a little

---
+++ 494/Final-Game/Assets/DisableCrystals.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisableCrystals : MonoBehaviour {
+	public GameObject crystals;
+	
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			crystals.active = false;
+		}
+	}
+}
---
+++ 494/Final-Game/Assets/EnableCrystals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableCrystals : MonoBehaviour {
+
+	public GameObject crystals;
+
+	void OnTriggerEnter(Collider col) {
+		if (col.tag == "Player") {
+			crystals.active = true;
+		}
+	}
+}
