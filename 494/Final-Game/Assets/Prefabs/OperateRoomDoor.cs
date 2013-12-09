1b8a5b269dd38b9e94510960051763f4eecf3697
Samantha Roach <samdbray@umich.edu>
2013-12-09 02:39:23 -0500

    changed OperateRoomDoor script to work both with the bedroom and closet door

--- 494/Final-Game/Assets/Prefabs/OperateRoomDoor.cs
+++ 494/Final-Game/Assets/Prefabs/OperateRoomDoor.cs
@@ -2,85 +2,123 @@
 using System.Collections;
 
 public class OperateRoomDoor : MonoBehaviour {
-	private float closedEulerAngleY = 180.0f;
-	private float openedEulerAngleY = 45.0f;
-	private float turnSpeed = 50.0f;
-	private bool isOpen = false;
-	private bool isClosed = true;
-	private bool open = false;
-	private bool close = false;
-	private bool moving = false;
+  private GameObject player;
+  private Vector3 playerFwd;
+  private float hitRange = 3.0f;
+  private RaycastHit hit;
 
-	// Run as the scene is initializing
-	void Awake() {
-		
-	}
+  private float closedEulerAngleY = 180.0f;
+  private float openedEulerAngleY = 45.0f;
+  private float turnSpeed = 50.0f;
+  private bool isOpen = false;
+  private bool isClosed = true;
+  private bool open = false;
+  private bool close = false;
+  private bool moving = false;
 
-	// Use this for initialization
-	void Start() {
+  // Run as the scene is initializing
+  void Awake() {
+    player = GameObject.FindWithTag("Player");
+    // initialize the angles and turn speed used when closing and opening the 
+    // door from the door's current angles
+    closedEulerAngleY = transform.eulerAngles.y;
+    if(closedEulerAngleY <= 180.0f) {
+      openedEulerAngleY = closedEulerAngleY - 135.0f;
+    } else {
+      openedEulerAngleY = closedEulerAngleY - 235.0f;
+      turnSpeed *= -1.0f;
+    } 
+  }
 
-	}
-	
-	// Update is called once per frame
-	void Update() {
-		// open the door
-		if(open) {
-			if(transform.eulerAngles.y > openedEulerAngleY) {
-				transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
-			} else {
-				if(moving) {
-					moving = false;
-					SetToOpen();
-				}
-				open = false;
-				isOpen = true;
-				isClosed = false;
-			}
-		}
+  // Use this for initialization
+  void Start() {
 
-		// close the door
-		if(close) {
-			if(transform.eulerAngles.y < closedEulerAngleY) {
-				transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
-			} else {
-				if(moving) {
-					moving = false;
-					SetToClose();
-				}
-				close = false;
-				isClosed = true;
-				isOpen = false;
-			}
-		}
-	}
+  }
+  
+  // Update is called once per frame
+  void Update() {
+    // get Player's current forward vector
+    playerFwd = player.transform.forward;
 
-	public bool IsOpen() {
-		return isOpen;
-	}
+    // check to see if Player opened or closed the door
+    MoveDoor();
 
-	public bool IsClosed() {
-		return isClosed;
-	}
+    // open the door
+    if(open) {
+      if(transform.eulerAngles.y > openedEulerAngleY) {
+        transform.Rotate(Vector3.forward, -turnSpeed * Time.deltaTime);
+      } else {
+        if(moving) {
+          moving = false;
+          SetToOpen();
+        }
+        open = false;
+        isOpen = true;
+        isClosed = false;
+      }
+    }
 
-	public void OpenDoor() {
-		open = true;
-		moving = true;
-	}
+    // close the door
+    if(close) {
+      if(transform.eulerAngles.y < closedEulerAngleY) {
+        transform.Rotate(Vector3.forward, turnSpeed * Time.deltaTime);
+      } else {
+        if(moving) {
+          moving = false;
+          SetToClose();
+        }
+        close = false;
+        isClosed = true;
+        isOpen = false;
+      }
+    }
+  }
 
-	public void CloseDoor() {
-		close = true;
-		moving = true;
-	}
+  void MoveDoor() {
+    if(Physics.Raycast(player.transform.position, playerFwd, out hit, hitRange)) {
+      if(hit.collider.gameObject.name == name) {
+        if(Input.GetButtonDown("A_1") || Input.GetMouseButtonDown(0)) {
+          // close the door if it is open
+          if(isOpen) {
+            CloseDoor();
+          }
 
-	public void SetToOpen() {
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, 
-																				openedEulerAngleY, 
-																				transform.eulerAngles.z);
-	}
+          // open the door if it is closed
+          if(isClosed) {
+            OpenDoor();
+          }
+        }
+      }
+    }
+  }
 
-	public void SetToClose() {
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, 
-																				closedEulerAngleY, 
-																				transform.eulerAngles.z);
-	}
+  public bool IsOpen() {
+    return isOpen;
+  }
+
+  public bool IsClosed() {
+    return isClosed;
+  }
+
+  public void OpenDoor() {
+    open = true;
+    moving = true;
+  }
+
+  public void CloseDoor() {
+    close = true;
+    moving = true;
+  }
+
+  public void SetToOpen() {
+    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 
+                                        openedEulerAngleY, 
+                                        transform.eulerAngles.z);
+  }
+
+  public void SetToClose() {
+    transform.eulerAngles = new Vector3(transform.eulerAngles.x, 
+                                        closedEulerAngleY, 
+                                        transform.eulerAngles.z);
+  }
 }
