5777b31ad9523a0645a4984837abd5860e7e92a6
Bradley Menchl <brmenchl@umich.edu>
2013-12-10 09:05:14 -0500

    a few changes.

--- 494/Final-Game/Assets/DontDestroyScript.cs
+++ 494/Final-Game/Assets/DontDestroyScript.cs
@@ -3,13 +3,22 @@ using System.Collections;
 
 public class DontDestroyScript : MonoBehaviour {
 	static int instance_count = 0;
+	private bool cutsceneDone;
+	public void setCutscene(bool _cut) {
+		cutsceneDone = _cut;
+	}
+	public bool getCutscene() {
+		return cutsceneDone;
+	}
 	private GameObject player;
+
 	void Awake () {
 		if(instance_count == 0) {
 			DontDestroyOnLoad(gameObject);
 			player = GameObject.FindWithTag("Player");
 			transform.position = player.transform.position;
 			transform.rotation = player.transform.rotation;
+			cutsceneDone = false;
 			instance_count++;
 		}
 		else {
