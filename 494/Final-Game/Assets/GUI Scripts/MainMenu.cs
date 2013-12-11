1ba80121e41c8694487a4a3abd11a15bf3958490
Shafeen M <sodl02@gmail.com>
2013-12-10 22:19:44 -0500

    Added flickering light on teddy and lined up menu options

--- 494/Final-Game/Assets/GUI Scripts/MainMenu.cs
+++ 494/Final-Game/Assets/GUI Scripts/MainMenu.cs
@@ -5,18 +5,24 @@ public class MainMenu : MonoBehaviour {
 	public GUIStyle titleStyle;
 	public GUIStyle buttonStyle;
 
+
 	public float menuButtonSeparation;
 	public float menuTitleOffset;
+	public float menuTitleLeftMarginOffset;
 	public float menuOptionsOffset;
+	public float menuLeftMarginOffset;
+
+	public GameObject flickerLightObject;
+	public float lightIntensity;
 
 	//private MenuCursor cursor;
 	void Start() {
 	//	cursor = GameObject.Find("cursor").GetComponent<MenuCursor>();
 	}
 	void OnGUI () {
-		GUI.Label (new Rect (Screen.width/2-50, 40+menuTitleOffset, 100, 100), 
+		GUI.Label (new Rect (Screen.width/2-50+menuTitleLeftMarginOffset, 40+menuTitleOffset, 100, 100), 
 		           "Time Out!", titleStyle);
-		if(GUI.Button(new Rect(Screen.width/2-50,120+menuOptionsOffset,100,30),
+		if(GUI.Button(new Rect(Screen.width/2-50+menuLeftMarginOffset,120+menuOptionsOffset,100,30),
 		              "Start New Game", buttonStyle)){
 			PlayerPrefs.DeleteKey("level1");
 			PlayerPrefs.DeleteKey("level2");
@@ -25,22 +31,22 @@ public class MainMenu : MonoBehaviour {
 			PlayerPrefs.DeleteKey("level5");
 			Application.LoadLevel("BedroomLevel");
 		}
-		if(GUI.Button(new Rect(Screen.width/2-50,150+1*menuButtonSeparation+menuOptionsOffset,100,30), 
+		if(GUI.Button(new Rect(Screen.width/2-50+menuLeftMarginOffset,150+1*menuButtonSeparation + menuOptionsOffset,100,30), 
 		              "Settings", buttonStyle)){
 			Application.LoadLevel("Settings");
 		}
-		if(GUI.Button(new Rect(Screen.width/2-50,180+2*menuButtonSeparation+menuOptionsOffset,100,30), 
+		if(GUI.Button(new Rect(Screen.width/2-50+menuLeftMarginOffset,180+2*menuButtonSeparation + menuOptionsOffset,100,30), 
 		              "Quit", buttonStyle)){
 			Application.Quit();
 		}
 
 		if (PlayerPrefs.HasKey("level2")) {
-			if(GUI.Button(new Rect(Screen.width/2-50,250+3*menuOptionsOffset + menuOptionsOffset,100,30), "Resume", buttonStyle)) {
+			if(GUI.Button(new Rect(Screen.width/2-50+menuLeftMarginOffset,210+3*menuButtonSeparation + menuOptionsOffset,100,30), "Resume", buttonStyle)) {
 				Application.LoadLevel("BedroomLevel");
 			}
 		}
 
-		if(GUI.Button(new Rect(Screen.width/2-50,300+4*menuOptionsOffset + menuOptionsOffset,100,30), "Start Showcase", buttonStyle)) {
+		if(GUI.Button(new Rect(Screen.width/2-50+menuLeftMarginOffset,240+4*menuButtonSeparation + menuOptionsOffset,100,30), "Start Showcase", buttonStyle)) {
 			PlayerPrefs.SetInt("level1", 1);
 			PlayerPrefs.SetInt("level2", 1);
 			PlayerPrefs.SetInt("level3", 1);
@@ -50,17 +56,12 @@ public class MainMenu : MonoBehaviour {
 		}
 
 	}
+
+
+
+
+
 	void Update() {
-		/*if(Input.GetButtonDown("A_1") || Input.GetMouseButtonDown(0)) {
-			if(startButton.HitTest(new Vector3(cursor.position.x + cursor.position.width/2,cursor.position.y + cursor.position.height/2,0))) {
-				Application.LoadLevel("Bedroom");
-			}
-			if(settingsButton.HitTest(new Vector3(cursor.position.x + cursor.position.width/2,cursor.position.y + cursor.position.height/2,0))) {
-				Application.LoadLevel("Settings");
-			}
-			if(quitButton.HitTest(new Vector3(cursor.position.x + cursor.position.width/2,cursor.position.y + cursor.position.height/2,0))) {
-				Application.Quit();
-			}
-		}*/
+		flickerLightObject.light.intensity = Random.Range(1,80);
 	}
 }
